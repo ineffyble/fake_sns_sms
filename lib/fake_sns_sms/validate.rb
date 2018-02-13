cbf3f482c256c071562ad3bafd76ac7b3368267b
Effy Elden <effye@thoughtworks.com>
2018-02-13 12:02:32 +1100

    Code cleanup

--- lib/fake_sns_sms/validate.rb
+++ lib/fake_sns_sms/validate.rb
@@ -6,13 +6,11 @@ module FakeSnsSms
   end
 
   def self.has_required_params?(params)
-    return false unless params[:PhoneNumber] && params[:MessageStructure] && params[:Message]
-    return true
+    params[:PhoneNumber] && params[:MessageStructure] && params[:Message]
   end
 
   def self.valid_phone_number?(pn)
     phone_number = Phonelib.parse(pn)
-    return false unless phone_number.full_e164 == pn
-    return true
+    phone_number.full_e164 == pn
   end
 end
